f481e42e85f604561218af3da06f0b0238671bac
sdpo-tarapur <sdpo.tatapur.munger@gmail.com>
2026-08-10 00:45:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  const repoName = process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/report/';
   return {
     base: './',
     plugins: [react(), tailwindcss()],
