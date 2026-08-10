0480a384d2fc19605529ccc1be7f9e36c4fe1bf5
sdpo-tarapur <sdpo.tatapur.munger@gmail.com>
2026-08-10 14:38:58 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  const repoName = process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-    : '/report/';
   return {
     base: './',
     plugins: [react(), tailwindcss()],
